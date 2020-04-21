9061d44dc1ed1c4347d2168cb4e46b66fe11004c
Chethan B <chethan25.bpac@gmail.com>
2020-04-21 21:40:54 +0530

    store all season in a dict

--- scraper.py
+++ scraper.py
@@ -2,50 +2,69 @@
 """
 Module to get and parse the tv series episodes name from imdb website
 """
+import os
 import sys
-
+import time
 import requests
 
 from bs4 import BeautifulSoup
 
-# Tv series imdb website url
-url = 'https://www.imdb.com/title/tt0096697/episodes?season=4'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0)\
-    Gecko/20100101 Firefox/74.0'
-    }
+#from main import path
 
-try:
-    req = requests.get(url, headers=headers, timeout=2)
-    req.raise_for_status()
-except requests.exceptions.HTTPError:
-    raise SystemExit('An HTTP error occurred')
-except requests.exceptions.ConnectionError:
-    raise SystemExit('A Connection error occurred')
-except requests.exceptions.Timeout:
-    raise SystemExit('The request timed out')
-except requests.exceptions.RequestException:
-    raise SystemExit('There was an ambiguous exception that occurred\
-        while handling your request')
-else:
-    soup = BeautifulSoup(req.text, 'lxml')
+path = "/media/chethan/WD My Passport Ultra/The Simpsons/"
+no_of_seasons = len(os.listdir(path))
 
-    episode_no_odd, episode_no_even = 1, 2
+season_dict = {}
 
-    episode_dict = {}
-    episode_list = []
+for season in range(1, no_of_seasons + 1):
+    # Tv series imdb website url
+    url = f'https://www.imdb.com/title/tt0096697/episodes?season={season}'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0)\
+        Gecko/20100101 Firefox/74.0'
+        }
 
-    for tag in soup.find_all('div', class_='list_item odd'):
-        tag1 = tag.find('div', class_='info')
-        episode_name_odd = tag1.a.text
-        episode_dict[episode_no_odd] = episode_name_odd
-        episode_no_odd += 2
+    try:
+        req = requests.get(url, headers=headers, timeout=2)
+        req.raise_for_status()
+    except requests.exceptions.HTTPError:
+        raise SystemExit('An HTTP error occurred')
+    except requests.exceptions.ConnectionError:
+        raise SystemExit('A Connection error occurred')
+    except requests.exceptions.Timeout:
+        raise SystemExit('The request timed out')
+    except requests.exceptions.RequestException:
+        raise SystemExit('There was an ambiguous exception that occurred\
+            while handling your request')
+    else:
+        soup = BeautifulSoup(req.text, 'lxml')
 
-    for tag in soup.find_all('div', class_='list_item even'):
-        tag1 = tag.find('div', class_='info')
-        episode_name_even = tag1.a.text
-        episode_dict[episode_no_even] = episode_name_even
-        episode_no_even += 2
+        episode_no_odd, episode_no_even = 1, 2
 
-    for episode in sorted(episode_dict.keys()):
-        episode_list.append(f'{episode} - {episode_dict[episode]}')
+        episode_dict = {}
+        episode_list = []
+
+        for tag in soup.find_all('div', class_='list_item odd'):
+            tag1 = tag.find('div', class_='info')
+            episode_name_odd = tag1.a.text
+            episode_dict[episode_no_odd] = episode_name_odd
+            episode_no_odd += 2
+
+        for tag in soup.find_all('div', class_='list_item even'):
+            tag1 = tag.find('div', class_='info')
+            episode_name_even = tag1.a.text
+            episode_dict[episode_no_even] = episode_name_even
+            episode_no_even += 2
+
+        for episode in sorted(episode_dict.keys()):
+            episode_list.append(f'{episode} - {episode_dict[episode]}')
+
+        
+
+        season_dict[season] = episode_list
+        print(f'Season {season} extracted')
+        print(season_dict)
+        time.sleep(req.elapsed.total_seconds())
+print(season_dict)
+        
+
