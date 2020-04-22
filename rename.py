68b12e12b792d78ca9f975c823924def174981cd
Chethan B <chethan25.bpac@gmail.com>
2020-04-22 20:20:13 +0530

    split the files

---
+++ rename.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Module to rename the files
+"""
+import os
+import sys
+
+import scraper
+from main import path
+from filenames_sort import sorted_alphanumeric
+
+
+new_season_dict = scraper.season_dict
+
+for season_no, episode_list in new_season_dict.items():
+    new_path = path + 'S' + str(season_no) + '/'
+    episode = 0
+    for episode_name in sorted_alphanumeric(os.listdir(new_path)):
+        # Assign src with files path name
+        src = new_path + episode_name
+        # Assign destination with path name and new filename
+        # based on files extension
+        desmp4 = new_path + episode_list[episode] + '.mp4'
+        desmkv = new_path + episode_list[episode] + '.mkv'
+        desavi = new_path + episode_list[episode] + '.avi'
+
+        # Rename files based on its extension
+        if episode_name[-3:] == 'mp4':
+            try:
+                os.rename(src, desmp4)
+            except FileNotFoundError:
+                raise SystemExit('Invalid path')
+            else:
+                episode += 1
+
+        if episode_name[-3:] == 'mkv':
+            try:
+                os.rename(src, desmkv)
+            except FileNotFoundError:
+                raise SystemExit('Invalid path')
+            else:
+                episode += 1
+
+        if episode_name[-3:] == 'avi':
+            try:
+                os.rename(src, desavi)
+            except FileNotFoundError:
+                raise SystemExit('Invalid path')
+            else:
+                episode += 1
